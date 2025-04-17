06ee06061175a265ee96e470f43e134bd501607a
Tiyasha <tiyashamohanty22@gmail.com>
2025-04-17 20:19:38 +0530

    jsx,functional component

--- App.js
+++ App.js
@@ -1,21 +1,59 @@
 import React from "react";
+import ReactDOM from "react-dom/client";
+//react element
+const jsxHeading = <h1>Hello from JSX</h1>;
 
-import { createRoot } from "react-dom/client";
+//react functional component
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "Namaste React"),
-    React.createElement("h2", {}, "Namaste React"),
-  ])
+const Title = function () {
+  return <h2>Namaste React</h2>;
+};
+const HeadingComponent = () => (
+  <div>
+    <h1>Hi from Functional component</h1>
+    <Title />
+  </div>
+);
+// ● Create a Nested header Element using React.createElement(h1,h2,h3 inside a
+//   div with class “title”)
+//   ○ Create the same element using JSX
+//   ○ Create a functional component of the same with JSX
+//   ○ Pass attributes into the tag in JSX
+//   ○ Composition of Component(Add a component inside another)
+//   ○ {TitleComponent} vs {<TitleComponent/>} vs
+//   {<TitleComponent></TitleComponent>} in JSX
+
+//   ● Create a Header Component from scratch using Functional Components with
+//   JSX
+//   ○ Add a Logo on left
+//   ○ Add a search bar in middle
+//   ○ Add User icon on right
+//   ○ Add CSS to make it look nice
+
+const header = React.createElement("div", { className: "title" }, [
+  React.createElement("h1", { id: "h1" }, "hello"),
+  React.createElement("h2", { id: "h2" }, "hello"),
+  React.createElement("h3", { id: "h3" }, "hello"),
+]);
+const headerJSX = (
+  <div className="title">
+    <h1>hellow</h1>
+    <h2>hi</h2>
+    <h3>hey</h3>
+  </div>
 );
 
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello world from React 2"
-);
-console.log(heading);
-const root = createRoot(document.getElementById("root"));
-root.render(parent);
+const HeaderComponent = function () {
+  return (
+    <div className="title">
+      <Title />
+      <h1>hellow</h1>
+      <h2>hi</h2>
+      <h3>hey from C</h3>
+    </div>
+  );
+};
+const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(jsxHeading);
+// root.render(<HeadingComponent />);
+root.render(<HeaderComponent />);
