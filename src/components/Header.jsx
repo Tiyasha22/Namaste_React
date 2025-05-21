b8023171f1205f9115e49b221821b7afeca83147
Tiyasha <tiyashamohanty22@gmail.com>
2025-05-21 12:58:27 +0530

    "react-router,custom hook and more"

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,8 @@
 import { LOGO_URL } from "../utils/constant";
-
+import { useState } from "react";
+import { Link } from "react-router-dom";
 const Header = () => {
+  const [btnLogin, setBtnLogin] = useState("Login");
   return (
     <div className="header">
       <div>
@@ -8,9 +10,21 @@ const Header = () => {
       </div>
       <div className="nav-items">
         <ul>
-          <li>Home</li>
-          <li>About Us</li>
-          <li>Cart</li>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="about">About Us</Link>
+          </li>
+          <li>
+            <Link to="contact">Contact</Link>
+          </li>
+          <li>
+            <Link to="cart">Cart</Link>
+          </li>
+          <button className="login" onClick={() => setBtnLogin("Logout")}>
+            {btnLogin}
+          </button>
         </ul>
       </div>
     </div>
